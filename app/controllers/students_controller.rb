2635b61c48cd5b337884a87c44139b9484cf3e7d
Will Hodgson <will.hodgson@current-rms.com>
2019-10-22 13:48:04 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -15,7 +15,7 @@ class StudentsController < ApplicationController
   end
 
   def edit
-    @student = Student.find(params[:id])
+    @student = Student.find_or_create_by(params[:id])
   end
 
   def update
