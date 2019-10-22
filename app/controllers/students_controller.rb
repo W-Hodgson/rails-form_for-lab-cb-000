34e107983ea0349ffe4221050c671d97f57507a5
Will Hodgson <will.hodgson@current-rms.com>
2019-10-22 12:23:59 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,7 +10,7 @@ class StudentsController < ApplicationController
   def create
     @student = Student.new(params.require(:students).permit(:first_name, :last_name))
     @student.save
-    redirect_to show_student_path
+    redirect_to student_path(@student)
   end
 
   def edit
