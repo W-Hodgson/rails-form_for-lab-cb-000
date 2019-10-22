eb61498d782515cd118950b1ef7582e76f719e9f
Will Hodgson <will.hodgson@current-rms.com>
2019-10-22 12:17:43 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,3 +1,21 @@
 class SchoolClassesController < ApplicationController
-  
+
+  def index
+    @students = Student.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def show
+  end
 end
