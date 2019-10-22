e4d6f2d7dda6865ffea4b7d5abf92036fdc798c2
Will Hodgson <will.hodgson@current-rms.com>
2019-10-22 13:29:17 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -9,7 +9,7 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
-    @school_class = SchoolClass.new(params.require(:school_class).permit(:title, :room_))
+    @school_class = SchoolClass.new(params.require(:school_class).permit(:title, :room_number))
   end
 
   def edit
