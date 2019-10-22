c415f9423d76e953a42f51136cac2f0e68eb99b3
Will Hodgson <will.hodgson@current-rms.com>
2019-10-22 13:36:48 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -19,9 +19,9 @@ class SchoolClassesController < ApplicationController
   end
 
   def update
-    @student = Student.find(params[:id])
-    @student.update(params.require(:post).permit(:first_name, :last_name))
-    redirect_to student_path(@student)
+    @school_class = Student.find(params[:id])
+    @school_class.update(params.require(:post).permit(:first_name, :last_name))
+    redirect_to student_path(@school_class)
   end
 
   def show
